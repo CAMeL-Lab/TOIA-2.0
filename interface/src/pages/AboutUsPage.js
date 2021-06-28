54bfad724d8d1806470f9a90f49d6364656d9f3d
NYUAD <nyuad@C2-LIB-MBP-16.local>
2021-06-29 01:33:04 +0400

    Added new camel lab logo

--- interface/src/pages/AboutUsPage.js
+++ interface/src/pages/AboutUsPage.js
@@ -13,14 +13,14 @@ import goffredo from "../images/goffredo.jpeg";
 import tyeece from "../images/Tyeece.jpg";
 import armaan from "../images/armaan.jpg";
 import nyuad from "../images/nyuad-rb.png";
-import camel from "../images/camel-lab-rb.png";
+import camel from "../images/camel.png";
 import history from '../services/history';
 import {Modal} from 'semantic-ui-react';
 
 
 function AvatarViewPage() {
 
-    /*functions in charge of opening and closing the various pop up menus*/  
+    /*functions in charge of opening and closing the various pop up menus*/
     const [open, dispatch] = useState(false);
 
     function openModal(e){
@@ -32,16 +32,16 @@ function AvatarViewPage() {
     var input1, input2;//holds email and pass from popup
 
     var team =[// This is a list of all members names and their accompanying pictures
-        
+
         { still: alberto, member: "Alberto C."},
         { still: tyeece, member: "Tyeece H."},
         { still: wahib, member: "Wahib K."},
         { still: kertu, member: "Kertu K."},
         { still: armaan, member: "Armaan A."},
-        { still: goffredo, member: "Goffredo P."}, 
+        { still: goffredo, member: "Goffredo P."},
         { still: erin, member: "Erin C."},
         { still: nizar, member: "Nizar H."},
-        
+
     ]
 
     const renderTeam = (card, index) => {//cards for members
@@ -51,7 +51,7 @@ function AvatarViewPage() {
                 />
                 <div>
                     <h1 className="about-name" //name of person
-                    >{card.member}</h1> 
+                    >{card.member}</h1>
                 </div>
             </div>
         )
@@ -62,13 +62,13 @@ function AvatarViewPage() {
           pathname: '/',
         });
       }
-    
+
       function library() {
         history.push({
           pathname: '/library',
         });
       }
-    
+
       function garden(e) {
         if (isLogin) {
           history.push({
@@ -105,15 +105,15 @@ function AvatarViewPage() {
             width: '600px',
         }
       };
-     
+
     //a function will be needed to send input5 to database (wahib)
-    
+
     return (
         <div className="about-page">
             <Modal //this is the new pop up menu
                 size='large'
                 style={inlineStyle.modal}
-                open={open} 
+                open={open}
                 onClose={() => dispatch(false)}
             >
                     <Modal.Header className="login_header">
@@ -163,7 +163,7 @@ function AvatarViewPage() {
             </div>
             <div className = "about-team">
             <h1 className="about-heading">Meet The Team</h1>
-            <p className="about-text">The collection of researchers, both graduate and undegraduate, designers 
+            <p className="about-text">The collection of researchers, both graduate and undegraduate, designers
                 and intelligent minds that came together to make TOIA possible </p>
             <div className ="about-grid" //videos
             >
@@ -176,22 +176,22 @@ function AvatarViewPage() {
             <a href="https://github.com/nizarhabash1/TOIA-NYUAD"><i className="fa fa-github gitImage"></i></a>
             <a href="https://nyuad.nyu.edu/en/research/faculty-labs-and-projects/computational-approaches-to-modeling-language-lab.html"><img src={camel} className = "camelImage"/></a>
             </div>
-            
-            
+
+
             <div className = "publications">
             <p className="publication-links">Publication Links </p>
             <ul>
                 <li>
                 Alberto Chierici, Tyeece Hensley, Wahib Kamran, Kertu Koss, Armaan Agrawal, Erin Collins, Goffredo Puccetti and Nizar Habash, A Cloud-based User-Centered Time-Offset Interaction Application,
                 SIGdial, April 2021 <a href="https://www.aclweb.org/anthology/2020.lrec-1.60.pdf">[PDF]</a>
-                
+
                 </li>
-                <li > 
+                <li >
                 Nizar Habash and Alberto Chierchi, A View From the Crowd: Evaluation Challenges for Time-Offset Interaction Applications,
                 Association for Computational Linguistics, April 2021, <a href="https://www.aclweb.org/anthology/2021.humeval-1.9.pdf">[PDF]</a>
                 <a href="https://www.aclweb.org/anthology/2021.humeval-1.9.bib">[BIB]</a>
                 </li>
-                <li> 
+                <li>
                 Alberto Chierici, Nizar Habash, Margarita Bicec, The Margarita Dialogue Corpus: A Data Set for Time-Offset Interactions and Unstructured Dialogue Systems,
                 Proceedings of the 12th Language Resources and Evaluation Conference, May 2020, <a href="https://www.aclweb.org/anthology/2020.lrec-1.60.pdf">[PDF]</a>
                 <a href="https://www.aclweb.org/anthology/2020.lrec-1.60.bib">[BIB]</a>
@@ -204,10 +204,10 @@ function AvatarViewPage() {
             </ul>
 
             </div>
-            
+
         </div>
-        
-        
+
+
     );
 }
 
