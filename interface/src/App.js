ce2c21f146fa857f9468dc766a417532d57fb5d0
Armaan Agrawal <aa7385@nyu.edu>
2021-07-07 02:07:57 +0400

    Favicon and Title

--- interface/src/App.js
+++ interface/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
 
@@ -6,6 +6,10 @@ import history from './services/history';
 import Routes from './routes/';
 
 function App() {
+  useEffect(() => {
+    
+    document.title = `TOIA`;
+  });
   return (
     <Router history={history}>
       <Routes />
