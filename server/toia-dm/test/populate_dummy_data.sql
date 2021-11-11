c9fe71d931a7a81c8d2b5551f299e3ec95b5b131
AMChierici <alberto.chierici@gmail.com>
2021-11-11 10:57:22 +0400

    added script to generate dummy data in dev database

---
+++ server/toia-dm/test/populate_dummy_data.sql
@@ -0,0 +1,17 @@
+INSERT INTO toia_user(id,first_name,last_name,language,email,password) VALUES (1,"Jon","Doe","en-US","jon.doe@gmail.com","abc123");
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("ef1","answer",1,1,0,"How are you?","I am fine thanks!","en-US",5,14);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("0b2","answer",1,2,0,"What is your favorite sport?","I love soccer!","en-US",2,5);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("a7c","answer",1,3,0,"What do you study?","I study Computer Science.","en-US",10,34);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("o8i","answer",1,4,1,"Where do you live?","I live in Abu Dhabi.","en-US",0,2);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r21","asnwer",1,5,0,"When is your birthday?","My birthday is in September.","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2t","y/n-answer",1,13,0,"Do you like sushi?","Yes","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r4t","y/n-answer",1,14,0,"Do you like swimming?","No","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2y","no-answer",1,15,0,"","I don't have an answer for that","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2e","greeting",1,16,0,"hey","how's it going?","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2m","greeting",1,6,0,"Howdy","hey, I'm all right","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2a","answer",1,7,0,"What do you do?","I am doing a Ph.D.","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2b","answer",1,8,0,"How old are you?","I'm 35. Age last birthday","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2c","answer",1,9,0,"Tell me something interesting?","I wrote a book about the ethics of artificial intelligence","en-US", 4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2d","filler",1,10,0,"When is your birthday?","My birthday is in September.","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2f","greeting",1,11,0,"Hello","Hi!","en-US",4,20);
+INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2h","no-answer",1,12,0,"","Sorry, I didn't get that","en-US",4,20);
