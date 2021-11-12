32436d0a67711e24d781563a001379701e38dea3
AMChierici <alberto.chierici@gmail.com>
2021-11-12 12:01:27 +0400

    minor edits

--- server/toia-dm/test/populate_dummy_data.sql
+++ server/toia-dm/test/populate_dummy_data.sql
@@ -1,4 +1,6 @@
+-- input a user
 INSERT INTO toia_user(id,first_name,last_name,language,email,password) VALUES (1,"Jon","Doe","en-US","jon.doe@gmail.com","abc123");
+-- input videos
 INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("ef1","answer",1,1,0,"How are you?","I am fine thanks!","en-US",5,14);
 INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("0b2","answer",1,2,0,"What is your favorite sport?","I love soccer!","en-US",2,5);
 INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("a7c","answer",1,3,0,"What do you study?","I study Computer Science.","en-US",10,34);
@@ -15,3 +17,22 @@ INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,lik
 INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2d","filler",1,10,0,"When is your birthday?","My birthday is in September.","en-US",4,20);
 INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2f","greeting",1,11,0,"Hello","Hi!","en-US",4,20);
 INSERT INTO video(id_video,type,toia_id,idx,private,question,answer,language,likes,views) VALUES("r2h","no-answer",1,12,0,"","Sorry, I didn't get that","en-US",4,20);
+-- input stream
+
+-- link videos to stream
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"ef1");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"0b2");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"a7c");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"o8i");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r21");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2t");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r4t");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2y");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2e");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2m");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2a");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2b");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2c");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2d");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2f");
+INSERT INTO stream_has_video(stream_id_stream,video_id_video) VALUES(1,"r2h");
