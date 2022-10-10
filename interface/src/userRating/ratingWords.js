a4417c672673e17bcabfb05f565371da184f5f43
bishnudev <devbishnu2000@gmail.com>
2022-10-10 13:14:56 +0400

    :bug: show rating after video has played

--- interface/src/userRating/ratingWords.js
+++ interface/src/userRating/ratingWords.js
@@ -1,70 +1,56 @@
-import * as React from "react";
-import Rating from "@mui/material/Rating";
-import Box from "@mui/material/Box";
 import StarIcon from "@mui/icons-material/Star";
+import Box from "@mui/material/Box";
+import Rating from "@mui/material/Rating";
+import * as React from "react";
 
 const labels = {
-  1: "Clearly not a good match",
-
-  2: "Poor match",
-
-  3: "Neutral",
-
-  4: "Good match",
-
-  5: "Perfect match for the context",
+    1: "Clearly not a good match",
+    2: "Poor match",
+    3: "Neutral",
+    4: "Good match",
+    5: "Perfect match for the context",
 };
 
 function getLabelText(value) {
-  return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
+    return `${value} Star${value !== 1 ? "s" : ""}, ${labels[value]}`;
 }
 
-export default function HoverRating({
-  setRatingValue,
-  setRatingComplete,
-  recUserRating,
-  newUserRatingVal,
-  skipFillVideo,
-}) {
-  const [value, setValue] = React.useState(1);
-  const [hover, setHover] = React.useState(-1);
-  // setRatingComplete(false);
+export default function HoverRating({ recUserRating }) {
+    const [value, setValue] = React.useState(1);
+    const [hover, setHover] = React.useState(-1);
 
-  return (
-    <Box
-      sx={{
-        width: 200,
-        display: "flex",
-        alignItems: "center",
-      }}
-    >
-      <Rating
-        name="hover-feedback"
-        size="large"
-        value={value}
-        precision={1}
-        max={5}
-        getLabelText={getLabelText}
-        onChange={(event, newValue) => {
-          if (newValue === null) newValue = value;
-          setValue(newValue);
-          setRatingValue(newValue);
-          setRatingComplete(true);
-          newUserRatingVal.current = "true";
-          skipFillVideo();
+    return (
+        <Box
+            sx={{
+                width: 200,
+                display: "flex",
+                alignItems: "center",
+            }}
+        >
+            <Rating
+                name="hover-feedback"
+                size="large"
+                value={value}
+                precision={1}
+                max={5}
+                getLabelText={getLabelText}
+                onChange={(event, newValue) => {
+                    if (newValue === null) newValue = value;
+                    setValue(newValue);
 
-          // recording the user rating
-          recUserRating(newValue);
-          console.log("rated!");
-        }}
-        onChangeActive={(event, newHover) => {
-          setHover(newHover);
-        }}
-        emptyIcon={<StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />}
-      />
-      {value !== null && (
-        <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
-      )}
-    </Box>
-  );
+                    // recording the user rating
+                    recUserRating(newValue);
+                }}
+                onChangeActive={(event, newHover) => {
+                    setHover(newHover);
+                }}
+                emptyIcon={
+                    <StarIcon style={{ opacity: 0.55 }} fontSize="inherit" />
+                }
+            />
+            {value !== null && (
+                <Box sx={{ ml: 2 }}>{labels[hover !== -1 ? hover : value]}</Box>
+            )}
+        </Box>
+    );
 }
