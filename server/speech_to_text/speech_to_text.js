bc4fc0687fd776170b619c0b668d40bcddb42de1
gautham.dk <gdk244@nyu.edu>
2022-11-14 10:53:07 +0400

    translation service added

--- server/speech_to_text/speech_to_text.js
+++ server/speech_to_text/speech_to_text.js
@@ -22,14 +22,16 @@ function constructRequest(inputLanguageCode){
 
 // the request object
 const request = {
-	config: {
-		encoding: encoding,
-		sampleRateHertz: sampleRateHertz,
-		languageCode: languageCode,
-		enableAutomaticPunctuation: true,
-		profanityFilter: true,
-	},
-	interimResults: true, // If you want interim results, set this to true
+    config: {
+      encoding: encoding,
+      sampleRateHertz: sampleRateHertz,
+      languageCode: languageCode,
+      enableAutomaticPunctuation: true,
+      profanityFilter: true,
+      enableWordTimeOffsets: true,
+      //"enable_separate_recognition_per_channel": false,
+    },
+    interimResults: true, // If you want interim results, set this to true
 };
 
 // config of the client object
---
+++ server/translation/generate_srt.py
@@ -0,0 +1,88 @@
+import json
+from google.cloud import storage
+import os
+from dotenv import load_dotenv
+
+from msg2srt import msg2srt
+from translate_txt import copy_to_local
+from txt2srt import txt2srt
+from translate_txt import batch_translate_text
+
+env = os.getenv('ENVIRONMENT')
+
+def generate_srt(data, target_langs, video_name, output_file="en", input_language="en-US", max_chars=40):
+
+    print(f"{env}: Generating SRT files from queue...")
+
+    load_dotenv()
+
+    INPUT_BUCKET = os.getenv('INPUT_BUCKET')
+    OUTPUT_BUCKET = os.getenv('OUTPUT_BUCKET')
+    INPUT_URI = os.getenv('INPUT_URI')
+    OUTPUT_URI = os.getenv('OUTPUT_URI')
+    PROJECT_ID = os.getenv('PROJECT_ID')
+    SUBTITLES = os.getenv('SUBTITLE_BUCKET')
+    TRANSCRIPTS = os.getenv('TRANSCRIPT_BUCKET')
+
+    transcript_txt = f'txts/{output_file}.txt'
+    transcript_srt = f'srts/{output_file}.srt'
+
+    msg2srt(data, max_chars, output_file, input_language)
+    copy_txt(output_file, INPUT_BUCKET, PROJECT_ID)
+    clear_bucket(OUTPUT_BUCKET, PROJECT_ID)
+    batch_translate_text(INPUT_URI, OUTPUT_URI, PROJECT_ID,
+                         input_language, target_lang=target_langs)
+    copy_to_local("txts", OUTPUT_BUCKET, PROJECT_ID)
+    txt2srt(transcript_srt, txt_out="txts", srt_out="srts")
+    if env == "PRODUCTION":
+        upload_to_bucket(SUBTITLES, TRANSCRIPTS, PROJECT_ID, video_name)
+    # elif env == "DEVELOPMENT":
+    #     upload_to_folders("Subtitles", video_name)
+    clear_folders()
+
+
+def copy_txt(output_file, bucket_in, project_id):
+    storage_client = storage.Client(project_id)
+    bucket = storage_client.get_bucket(bucket_in)
+    filename = f'{output_file}.txt'
+    blob = bucket.blob(filename)
+    blob.upload_from_filename(f'txts/{filename}')
+
+
+def clear_bucket(bucket_out, project_id):
+    storage_client = storage.Client(project_id)
+    blobs = storage_client.list_blobs(bucket_out)
+    for blob in blobs:
+        blob.delete()
+
+
+def upload_to_bucket(subtitles, transcripts, project_id, video_name):
+    storage_client = storage.Client(project_id)
+    bucket = storage_client.get_bucket(subtitles)
+
+    for f in os.listdir("srts"):
+        blob = bucket.blob(f'{video_name}-{f}')
+        blob.upload_from_filename(f'srts/{f}')
+
+    bucket = storage_client.get_bucket(transcripts)
+    for f in os.listdir("txts"):
+        if f.endswith('.csv'):
+            continue
+        parts = f.split('_')
+        if len(parts) > 1:
+            filename = parts[4]
+        else:
+            filename = f
+        blob = bucket.blob(f'{video_name}-{filename}')
+        blob.upload_from_filename(f'txts/{f}')
+
+def clear_folders():
+    dirs = ["txts", "srts"]
+    for dir in dirs:
+        for f in os.listdir(dir):
+            os.remove(os.path.join(dir, f))
+
+# def upload_to_folders(folder_name, video_name):
+#     for f in os.listdir("srts"):
+
+
