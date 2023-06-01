ac035f4c91116d72db86f55c5900888c09211ce3
bordernone <61447680+bordernone@users.noreply.github.com>
2023-06-01 13:56:39 +0400

    added OS metrics in dashbaord

--- api-v2/toia/mix.exs
+++ api-v2/toia/mix.exs
@@ -19,7 +19,7 @@ defmodule Toia.MixProject do
   def application do
     [
       mod: {Toia.Application, []},
-      extra_applications: [:logger, :runtime_tools]
+      extra_applications: [:logger, :runtime_tools, :os_mon]
     ]
   end
 
