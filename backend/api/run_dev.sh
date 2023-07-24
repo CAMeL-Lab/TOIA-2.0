a0c3d5d6f7c19d92196683628b968d39a3075169
Bishnu Dev <devbishnu2000@gmail.com>
2023-07-24 17:03:51 +0400

    container design changes

--- backend/api/run_dev.sh
+++ backend/api/run_dev.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+cd ".."
 sed -E 's|(.+)|export \1|g' .env > tmp_env
 source tmp_env
 rm tmp_env
+cd "api"
 mix deps.get
 mix ecto.setup
 mix phx.server
--- backend/speech-to-text/gcloud/config.js
+++ backend/speech-to-text/gcloud/config.js
@@ -1,10 +1,15 @@
 const path = require("path");
+const fs = require("fs");
 
-const requiredEnvVariables = [
-    "GOOGLE_SPEECH_API_CREDENTIALS_FILE",
-    "API_URL",
-    "NODE_ENV",
-];
+const requiredFiles = ["secrets/gcloud-credentials.json"];
+
+requiredFiles.forEach((file) => {
+    if (!fs.existsSync(path.join(__dirname, file))) {
+        throw new Error(`File ${file} is missing.`);
+    }
+});
+
+const requiredEnvVariables = ["API_URL", "NODE_ENV"];
 
 requiredEnvVariables.forEach((envVariable) => {
     if (!process.env[envVariable]) {
@@ -14,15 +19,14 @@ requiredEnvVariables.forEach((envVariable) => {
 
 process.env["GOOGLE_APPLICATION_CREDENTIALS"] = path.join(
     __dirname,
-    process.env.GOOGLE_SPEECH_API_CREDENTIALS_FILE
+    "secrets/gcloud-credentials.json"
 );
 
 const ENV = {
-    GOOGLE_SPEECH_API_CREDENTIALS_FILE: process.env.GOOGLE_SPEECH_API_CREDENTIALS_FILE, // Relative location of the credentials file
     GOOGLE_APPLICATION_CREDENTIALS: process.env.GOOGLE_APPLICATION_CREDENTIALS,
     API_URL: process.env.API_URL, // URL of the API where phoenix is running
     NODE_ENV: process.env.NODE_ENV, // Node environment
     PORT: 3002, // Port where the server will run
-}
+};
 
 module.exports = ENV;
