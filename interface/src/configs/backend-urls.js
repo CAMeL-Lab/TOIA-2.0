e5b3d7ba132b8322ccafca1bfd3bbb8e79465cf1
Bishnu Dev <devbishnu2000@gmail.com>
2023-08-03 21:26:41 +0545

    fix url, use watchtower

--- interface/src/configs/backend-urls.js
+++ interface/src/configs/backend-urls.js
@@ -1,4 +1,7 @@
-const BASE_URL = "http://localhost:4000/api";
+const BASE_URL =
+	process.env.NODE_ENV === "production"
+		? `/api`
+		: "http://localhost:4000/api";
 
 const API_URLS = {
 	SIGN_UP: () => `${BASE_URL}/auth/toia_user`,
@@ -6,10 +9,12 @@ const API_URLS = {
 	CREATE_NEW_STREAM: () => `${BASE_URL}/stream`,
 	GET_USER_STREAMS: user_id => `${BASE_URL}/toia_user/${user_id}/streams`,
 	VIDEO_INFO: video_id => `${BASE_URL}/video/${video_id}`,
-	ONBOARDING_QUESTIONS_LIST: () => `${BASE_URL}/toia_user/questions/onboarding`,
+	ONBOARDING_QUESTIONS_LIST: () =>
+		`${BASE_URL}/toia_user/questions/onboarding`,
 	QUESTION_SUGGESTIONS: () => `${BASE_URL}/question_suggestions`,
 	ANSWERED_QUESTIONS: () => `${BASE_URL}/question/answered`,
-	STREAM_ANSWERED_QUESTIONS: stream_id => `${BASE_URL}/question/answered?stream_id=${stream_id}`,
+	STREAM_ANSWERED_QUESTIONS: stream_id =>
+		`${BASE_URL}/question/answered?stream_id=${stream_id}`,
 	USER_STATS: () => `${BASE_URL}/toia_user/stats`,
 	USER_INFO: () => `${BASE_URL}/toia_user`,
 	OTHER_USER_INFO: user_id => `${BASE_URL}/toia_user/${user_id}`,
@@ -26,12 +31,17 @@ const API_URLS = {
 		`${BASE_URL}/stream/${stream_id}/next?question=${encodeURIComponent(
 			question,
 		)}`,
-	SMART_QUESTIONS: stream_id => `${BASE_URL}/stream/${stream_id}/smart_questions`,
+	SMART_QUESTIONS: stream_id =>
+		`${BASE_URL}/stream/${stream_id}/smart_questions`,
 	STREAM_FILLER: stream_id => `${BASE_URL}/stream/${stream_id}/filler`,
 	SAVE_FEEDBACK: () => `${BASE_URL}/player_feedback`,
 };
 
-const SPEECH_TO_TEXT = () => "http://localhost:3002";
+const speech_to_text_host =
+	process.env.NODE_ENV === "production"
+		? `http://${window.location.hostname}`
+		: "http://localhost:3002";
+const SPEECH_TO_TEXT = () => speech_to_text_host;
 
 export default API_URLS;
 export { SPEECH_TO_TEXT };
