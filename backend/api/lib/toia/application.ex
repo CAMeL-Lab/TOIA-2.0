bf5cfdc5756ab97cce279723c5118a630f51b84a
Bishnu Dev <devbishnu2000@gmail.com>
2024-06-24 00:29:25 +0400

    fix cors

--- backend/api/lib/toia/application.ex
+++ backend/api/lib/toia/application.ex
@@ -2,6 +2,8 @@ defmodule Toia.Application do
   # See https://hexdocs.pm/elixir/Application.html
   # for more information on OTP Applications
   @moduledoc false
+  require HTTPoison
+  alias Jason, as: JSON
 
   use Application
   alias EmailHandlers
@@ -32,6 +34,7 @@ defmodule Toia.Application do
       [:swoosh, :deliver_many, :exception],
     ], &EmailHandlers.handle_event/4, nil)
 
+    set_gcloud_cors_on_startup()
     # See https://hexdocs.pm/elixir/Supervisor.html
     # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Toia.Supervisor]
@@ -44,5 +47,48 @@ defmodule Toia.Application do
   def config_change(changed, _new, removed) do
     ToiaWeb.Endpoint.config_change(changed, removed)
     :ok
+  end
+
+  def set_gcloud_cors_config(bucket_name) do
+    # Define your CORS configuration directly in code
+    cors_config = %{
+      cors: [
+        %{
+          origin: [System.get_env("API_URL")],
+          method: ["GET"],
+          responseHeader: ["Content-Type"],
+          maxAgeSeconds: 3600
+        }
+      ]
+    }
+
+    # Encode the configuration to a JSON string
+    body = JSON.encode!(cors_config)
+
+    # Construct the URL
+    url = "https://storage.googleapis.com/storage/v1/b/#{bucket_name}?fields=cors"
+
+    # Get the access token using Goth
+    {:ok, token} = Goth.Token.for_scope("https://www.googleapis.com/auth/devstorage.full_control")
+
+    # Set headers
+    headers = [
+      {"Authorization", "Bearer #{token.token}"},
+      {"Content-Type", "application/json"}
+    ]
+
+    # Make the PATCH request
+    HTTPoison.patch(url, body, headers)
+  end
+  
+  defp set_gcloud_cors_on_startup do
+    bucket_name = "toia-phoenix"
+
+    case set_gcloud_cors_config(bucket_name) do
+      {:ok, response} ->
+        IO.puts("CORS configuration set successfully: #{inspect(response)}")
+      {:error, reason} ->
+        IO.puts("Failed to set CORS configuration: #{inspect(reason)}")
+    end
   end
 end
